ba125dcfa432cabdcdba1e94ba952a5e8ef2e57a
AdelDima <adel4tahri@gmail.com>
2022-11-12 09:23:29 +0100

    📦 NEW: add producthant button light

--- components/navigation/Modal/YModel.tsx
+++ components/navigation/Modal/YModel.tsx
@@ -40,8 +40,8 @@ const YModel: React.FC<IYModel> = ({ ...glowProps }) => {
           href="https://www.producthunt.com/posts/youtube-playlist-to-notion?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-youtube&#0045;playlist&#0045;to&#0045;notion"
           target="_blank" rel="noreferrer"
         >
-          <Image
-            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=366369&theme=dark"
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=366369&theme=light"
             alt="Youtube&#0032;Playlist&#0032;To&#0032;Notion - Watch&#0032;your&#0032;favorite&#0032;Youtube&#0032;playlist&#0032;without&#0032;leaving&#0032;Notion | Product Hunt"
             width="190"
             height="50"
