5926fb57e7ce93468665901c52fe8091e8eb644d
AdelDima <adel4tahri@gmail.com>
2022-11-12 09:02:45 +0100

    🐛 FIX: Invalid next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  ignoreBuildErrors: true,
-  experimental: {
-    // transpilePackages: ["youtubei.js"],
-  },
-  // webpack5: true,
-  // webpack: (config) => {
-  //   config.resolve.fallback = { fs: false };
-  //   return config;
-  // },
-
-}
+};
 module.exports = nextConfig;
